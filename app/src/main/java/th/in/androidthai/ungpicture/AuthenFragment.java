71b14b65509627b4f10fcc69fb412936e4f560b4
masterUNG <phrombutr@gmail.com>
2019-05-09 10:31:58 +0700

    Design UI Authen OK

---
+++ app/src/main/java/th/in/androidthai/ungpicture/AuthenFragment.java
@@ -0,0 +1,54 @@
+package th.in.androidthai.ungpicture;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AuthenFragment extends Fragment {
+
+
+    public AuthenFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        SignUp Controller
+        signUpController();
+
+
+    }   // Main Method
+
+    private void signUpController() {
+        Button button = getView().findViewById(R.id.btnSignUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new RegisterFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_authen, container, false);
+    }
+
+}
--- app/src/main/java/th/in/androidthai/ungpicture/MainActivity.java
+++ app/src/main/java/th/in/androidthai/ungpicture/MainActivity.java
@@ -9,5 +9,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+//        Add Fragment to Activity
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.contentMainFragment, new AuthenFragment())
+                    .commit();
+        }
+
+
+    }   // Main Method
+
+}   // Main Class
